fa2444cf59956544f6764ef5bfe6d84bae1b478d
jimocool2 <muzan.jure@gmail.com>
2026-01-06 13:48:54 +0100

    Refactor BearerAuth security definition for improved clarity and format

--- app/__init__.py
+++ app/__init__.py
@@ -40,11 +40,13 @@ def create_app():
         "basePath": "/",
         "schemes": ["http", "https"],
         "securityDefinitions": {
-            "BearerAuth": {
-                "type": "apiKey",
-                "in": "header",
-                "name": "Authorization",
-                "description": "JWT Bearer token authentication. Format: 'Bearer <token>'"
+            'BearerAuth': {
+                'type': 'apiKey',
+                'name': 'Authorization',
+                'scheme': 'bearer',
+                'bearerFormat': 'bearer',
+                'in': 'header',
+                'description': 'Type in the *\'Value\'* input box below: **\'Bearer &lt;JWT&gt;\'**, where JWT is the token',
             }
         },
         "tags": [
